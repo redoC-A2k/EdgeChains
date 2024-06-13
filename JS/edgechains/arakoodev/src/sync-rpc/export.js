9af390b6078306d500c1c674c9ed7c7407c6d4de
afshan ahmed khan <afshanahmeda2k@gmail.com>
2024-06-13 16:04:34 +0530

    Add sync-rpc fork in edgechains

---
+++ JS/edgechains/arakoodev/src/sync-rpc/export.js
@@ -0,0 +1,9 @@
+console.log(process.env.arakoo)
+if (process.env.arakoo){
+    module.exports = function (path){
+        require(path)
+    }
+} else {
+    const createClient = require("./index.js");
+    module.exports = createClient;
+}
---
+++ JS/edgechains/arakoodev/src/sync-rpc/find-port.js
@@ -0,0 +1,14 @@
+// const getPort = require('get-port');
+// import getPort from "get-port";
+// const getPort = require("get-port");
+
+import("get-port").then(mod => {
+    let getPort = mod.default;
+    getPort()
+        .then(port => process.stdout.write('' + port))
+        .catch(err =>
+            setTimeout(() => {
+                throw err;
+            }, 0)
+        );
+})
